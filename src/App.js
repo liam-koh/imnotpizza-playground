bc31b6c566918b2d52145724d61a5a2aa52fe00b
imnotpizza <bobin6972@gmail.com>
2020-02-03 23:19:15 +0900

    02/03 집커밋

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Square from "./components/ex1.js"
+import Person from "./components/Person.js"
+
+/**
+ * props : 하위컴포넌트로 전달하는 값, vue와 동일
+ * state : 
+ * 
+ * 
+ */
+class App extends React.Component {
+  
 
 
-function App() {
-  return (
-    <div className="App">
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
         <div>
           <Square value={10}></Square>
+          <Person name={"name1"} age={26}></Person>
+          <Person name={"name2"} age={36}></Person>
+          <Person name={"name3"} age={46}></Person>
         </div>
       </header>
     </div>
-  );
+    )
+  }
+
+  
+  
 }
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// const Person = (props) => (
+//     <div>
+//         <h1>컴포넌트 props : {props.name}, age : {props.age}</h1>
+//     </div>
+// )
+
+// export default Person 
+
+class Person extends React.Component{
+    render(){
+
+        //const {name, age}=this.props; props의 name, age 할당
+
+        return(
+            <div>
+                <h1>props : {this.props.name}   age : {this.props.age}</h1>
+            </div>
+        )
+    }
+}
+
+export default Person;
